9c4a78e75fc3f873d1e5ce4e03eeb56b43e8ed9d
Nadezzzhda <nadezzzhda.omsk@gmail.com>
2026-03-11 18:41:48 +0400

    update brain-even part

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import greetings from '../src/brain-games.js'
-import userName from '../src/cli.js'
-import questionForUser from '../src/brain-even.js'
+import greetings from '../src/games/brain-games.js'
+import userName from '../src/index.js'
+import { questionForUser } from '../src/index.js'
+import { questionText, generateEvenData } from '../src/games/brain-even.js'
 
 greetings()
 const name = userName()
-questionForUser(name)
+questionForUser(name, questionText, generateEvenData)
